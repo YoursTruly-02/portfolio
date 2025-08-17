2db1ef54dbc9335b74a7718e234cb8624c875a8c
Fahad Hussain <01amfahad@gmail.com>
2025-08-17 22:25:47 +0530

    js

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,6 +1,14 @@
 /*==================== toggle icon navbar ====================*/
 /*==================== scroll sections active link ====================*/
 
+let sections = document.querySelector('section');
+let navLinks = document.querySelector('header nav a');
+
+window.onscroll=() => {
+  let top =window.scrollY;
+  let offset= sec.offsetTop-150;
+  let height= sec.offsetHeight;
+}
   /*==================== sticky navbar ====================*/
   /*==================== remove toggle icon and navbar when click navbar link (scroll) ====================*/
 
