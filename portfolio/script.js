f43d14b7cd899996562a3649f30a6ed34d429da0
Fahad Hussain <01amfahad@gmail.com>
2025-08-20 23:46:39 +0530

    c

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,23 +1,26 @@
 /*==================== toggle icon navbar ====================*/
 /*==================== scroll sections active link ====================*/
 
+
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
-
+console.log("Sections found:", sections);
 window.onscroll = () => {
   sections.forEach(sec => {
-  let top = window.scrollY;
-  let offset = sec.offsetTop - 150;
-  let height = sec.offsetHeight;
-  let id = sec.getAttribute('id');
-  
-  if (top >= offset && top < offset + height) {
-    navLinks.forEach(links => {
-      links.classList.remove('active');
-      document.querySelector('header nav a[href*=' + ']').classList.add('active');
-    })
-  };
-})
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach(links => {
+        links.classList.remove('active');
+
+        document.querySelector('header nav a[href*=' + id + '"]').classList.add('active');
+
+      });
+    };
+  });
 };
 /*==================== sticky navbar ====================*/
 /*==================== remove toggle icon and navbar when click navbar link (scroll) ====================*/
